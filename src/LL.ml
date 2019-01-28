7c9a7f14c849317f9ef814925022cc19a0c29764
Dhruv Makwana <dc_mak@outlook.com>
2019-01-28 20:11:39 +0000

    Refactor names

--- src/LL.ml
+++ src/LL.ml
@@ -38,7 +38,7 @@ type result =
   | Bool_op of bool_op
 ;;
 
-type parse_instruction = 
+type symbol = 
   | Eat of token
 
   | If_semicolon_then_stmt
@@ -68,7 +68,7 @@ type parse_instruction =
 exception Parse_empty
 ;;
 
-exception Parse_error of parse_instruction list * result list * token list
+exception Parse_error of symbol list * result list * token list
 ;;
 
 let (=) =
@@ -80,135 +80,135 @@ let rec loop = function
   | [], [Stmt ans], [] ->
     ans
 
-  (* Not modifying stack *)
+  (* Not modifying scratch *)
 
-  | Eat tok :: instr, stack, tok' :: rest ->
-    if tok = tok' then loop (instr, stack, rest) else raise @@ Parse_error (Eat tok :: instr, stack, tok' :: rest)
+  | Eat tok :: stack, scratch, tok' :: stream ->
+    if tok = tok' then loop (stack, scratch, stream) else raise @@ Parse_error (Eat tok :: stack, scratch, tok' :: stream)
 
   (* Statement *)
 
-  | Push_statement :: instr, stack, ID var :: rest ->
-    loop (Eat COLON_EQ :: Push_arith :: Make_assign :: If_semicolon_then_stmt :: instr, Arith (Var var) :: stack, rest)
+  | Push_statement :: stack, scratch, ID var :: stream ->
+    loop (Eat COLON_EQ :: Push_arith :: Make_assign :: If_semicolon_then_stmt :: stack, Arith (Var var) :: scratch, stream)
 
-  | Push_statement :: instr, stack, WHILE :: rest ->
-    loop (Push_bool :: Eat DO :: Push_statement :: Make_while :: If_semicolon_then_stmt :: instr, stack, rest)
+  | Push_statement :: stack, scratch, WHILE :: stream ->
+    loop (Push_bool :: Eat DO :: Push_statement :: Make_while :: If_semicolon_then_stmt :: stack, scratch, stream)
 
-  | Push_statement :: instr, stack, IF :: rest ->
-    loop (Push_bool :: Eat THEN :: Push_statement :: Eat ELSE :: Push_statement :: Make_if :: If_semicolon_then_stmt :: instr, stack, rest)
+  | Push_statement :: stack, scratch, IF :: stream ->
+    loop (Push_bool :: Eat THEN :: Push_statement :: Eat ELSE :: Push_statement :: Make_if :: If_semicolon_then_stmt :: stack, scratch, stream)
 
-  | Push_statement :: instr, stack, LEFT_CURLY :: rest ->
-    loop (Push_statement :: Eat RIGHT_CURLY :: If_semicolon_then_stmt :: instr, stack, rest)
+  | Push_statement :: stack, scratch, LEFT_CURLY :: stream ->
+    loop (Push_statement :: Eat RIGHT_CURLY :: If_semicolon_then_stmt :: stack, scratch, stream)
 
-  | If_semicolon_then_stmt :: instr, stack, SEMICOLON :: rest ->
-    loop (Push_statement :: Make_seq :: If_semicolon_then_stmt :: instr, stack, rest)
+  | If_semicolon_then_stmt :: stack, scratch, SEMICOLON :: stream ->
+    loop (Push_statement :: Make_seq :: If_semicolon_then_stmt :: stack, scratch, stream)
 
-  | If_semicolon_then_stmt :: instr, stack, rest ->
-    loop (instr, stack, rest)
+  | If_semicolon_then_stmt :: stack, scratch, stream ->
+    loop (stack, scratch, stream)
 
   (* Boolean expressions *)
 
-  | Push_bool :: instr, stack, TRUE :: rest ->
-    loop (If_and_or_or_then_bool :: instr, Bool True :: stack, rest)
+  | Push_bool :: stack, scratch, TRUE :: stream ->
+    loop (If_and_or_or_then_bool :: stack, Bool True :: scratch, stream)
 
-  | Push_bool :: instr, stack, FALSE :: rest ->
-    loop (If_and_or_or_then_bool :: instr, Bool False :: stack, rest)
+  | Push_bool :: stack, scratch, FALSE :: stream ->
+    loop (If_and_or_or_then_bool :: stack, Bool False :: scratch, stream)
 
-  | Push_bool :: instr, stack, BANG :: rest ->
-    loop (Push_bool :: Make_not :: If_and_or_or_then_bool :: instr, stack, rest)
+  | Push_bool :: stack, scratch, BANG :: stream ->
+    loop (Push_bool :: Make_not :: If_and_or_or_then_bool :: stack, scratch, stream)
 
-  | Push_bool :: instr, stack, LEFT_PAREN :: rest ->
-    loop (Push_bool :: Eat RIGHT_PAREN :: If_and_or_or_then_bool :: instr, stack, rest)
+  | Push_bool :: stack, scratch, LEFT_PAREN :: stream ->
+    loop (Push_bool :: Eat RIGHT_PAREN :: If_and_or_or_then_bool :: stack, scratch, stream)
 
-  | Push_bool :: instr, stack, ID var :: rest ->
-    loop (If_plus_or_star_then_arith :: Push_bool_op :: Push_arith :: Make_bool_op :: If_and_or_or_then_bool :: instr, (Arith (Var var)) :: stack, rest)
+  | Push_bool :: stack, scratch, ID var :: stream ->
+    loop (If_plus_or_star_then_arith :: Push_bool_op :: Push_arith :: Make_bool_op :: If_and_or_or_then_bool :: stack, (Arith (Var var)) :: scratch, stream)
 
-  | Push_bool :: instr, stack, NUM num :: rest ->
-    loop (If_plus_or_star_then_arith :: Push_bool_op :: Push_arith :: Make_bool_op :: If_and_or_or_then_bool :: instr, (Arith (Int num)) :: stack, rest)
+  | Push_bool :: stack, scratch, NUM num :: stream ->
+    loop (If_plus_or_star_then_arith :: Push_bool_op :: Push_arith :: Make_bool_op :: If_and_or_or_then_bool :: stack, (Arith (Int num)) :: scratch, stream)
 
-  | If_and_or_or_then_bool :: instr, stack, AND :: rest ->
-    loop (Push_bool :: Make_and :: If_and_or_or_then_bool :: instr, stack, rest)
+  | If_and_or_or_then_bool :: stack, scratch, AND :: stream ->
+    loop (Push_bool :: Make_and :: If_and_or_or_then_bool :: stack, scratch, stream)
 
-  | If_and_or_or_then_bool :: instr, stack, OR :: rest ->
-    loop (Push_bool :: Make_or :: If_and_or_or_then_bool :: instr, stack, rest)
+  | If_and_or_or_then_bool :: stack, scratch, OR :: stream ->
+    loop (Push_bool :: Make_or :: If_and_or_or_then_bool :: stack, scratch, stream)
 
-  | If_and_or_or_then_bool :: instr, stack, rest ->
-    loop (instr, stack, rest)
+  | If_and_or_or_then_bool :: stack, scratch, stream ->
+    loop (stack, scratch, stream)
 
-  | Push_bool_op :: instr, stack, LESS_THAN :: rest ->
-    loop (instr, (Bool_op Lt) :: stack, rest)
+  | Push_bool_op :: stack, scratch, LESS_THAN :: stream ->
+    loop (stack, (Bool_op Lt) :: scratch, stream)
 
-  | Push_bool_op :: instr, stack, EQUALS :: rest ->
-    loop (instr, (Bool_op Eq) :: stack, rest)
+  | Push_bool_op :: stack, scratch, EQUALS :: stream ->
+    loop (stack, (Bool_op Eq) :: scratch, stream)
 
-  | Push_bool_op :: instr, stack, GREATER_THAN :: rest ->
-    loop (instr, (Bool_op Gt) :: stack, rest)
+  | Push_bool_op :: stack, scratch, GREATER_THAN :: stream ->
+    loop (stack, (Bool_op Gt) :: scratch, stream)
 
   (* Arithmetic *)
 
-  | Push_arith :: instr, stack, NUM n :: rest ->
-    loop (If_plus_or_star_then_arith :: instr, (Arith (Int n)) :: stack, rest)
+  | Push_arith :: stack, scratch, NUM n :: stream ->
+    loop (If_plus_or_star_then_arith :: stack, (Arith (Int n)) :: scratch, stream)
 
-  | Push_arith :: instr, stack, ID var :: rest ->
-    loop (If_plus_or_star_then_arith :: instr, (Arith (Var var)) :: stack, rest)
+  | Push_arith :: stack, scratch, ID var :: stream ->
+    loop (If_plus_or_star_then_arith :: stack, (Arith (Var var)) :: scratch, stream)
 
-  | Push_arith :: instr, stack, LEFT_PAREN :: rest ->
-    loop (Push_arith :: If_plus_or_star_then_arith :: instr, stack, rest)
+  | Push_arith :: stack, scratch, LEFT_PAREN :: stream ->
+    loop (Push_arith :: If_plus_or_star_then_arith :: stack, scratch, stream)
 
-  | If_plus_or_star_then_arith :: instr, stack, PLUS :: rest ->
-    loop (Push_arith :: Make_plus :: If_plus_or_star_then_arith :: instr, stack, rest)
+  | If_plus_or_star_then_arith :: stack, scratch, PLUS :: stream ->
+    loop (Push_arith :: Make_plus :: If_plus_or_star_then_arith :: stack, scratch, stream)
 
-  | If_plus_or_star_then_arith :: instr, stack, STAR :: rest ->
-    loop (Push_arith :: Make_times :: If_plus_or_star_then_arith :: instr, stack, rest)
+  | If_plus_or_star_then_arith :: stack, scratch, STAR :: stream ->
+    loop (Push_arith :: Make_times :: If_plus_or_star_then_arith :: stack, scratch, stream)
 
-  | If_plus_or_star_then_arith :: instr, stack, rest ->
-    loop (instr, stack, rest)
+  | If_plus_or_star_then_arith :: stack, scratch, stream ->
+    loop (stack, scratch, stream)
 
-  (* Modifying stack *)
+  (* Modifying scratch *)
 
   (* Statments *)
 
-  | Make_assign :: instr, (Arith exp) :: (Arith (Var var)) :: stack, rest ->
-    loop (instr, Stmt (Assign (var, exp)) :: stack, rest)
+  | Make_assign :: stack, (Arith exp) :: (Arith (Var var)) :: scratch, stream ->
+    loop (stack, Stmt (Assign (var, exp)) :: scratch, stream)
 
-  | Make_while :: instr, (Stmt body) :: (Bool cond) :: stack, rest ->
-    loop (instr, Stmt (While (cond, body)) :: stack, rest)
+  | Make_while :: stack, (Stmt body) :: (Bool cond) :: scratch, stream ->
+    loop (stack, Stmt (While (cond, body)) :: scratch, stream)
 
-  | Make_seq :: instr, (Stmt s2) :: (Stmt s1) :: stack, rest ->
-    loop (instr, Stmt (Seq (s1, s2)) :: stack, rest)
+  | Make_seq :: stack, (Stmt s2) :: (Stmt s1) :: scratch, stream ->
+    loop (stack, Stmt (Seq (s1, s2)) :: scratch, stream)
 
-  | Make_if :: instr, (Stmt false_) :: (Stmt true_) :: (Bool cond) :: stack, rest ->
-    loop (instr, Stmt (If (cond, true_, false_)) :: stack, rest)
+  | Make_if :: stack, (Stmt false_) :: (Stmt true_) :: (Bool cond) :: scratch, stream ->
+    loop (stack, Stmt (If (cond, true_, false_)) :: scratch, stream)
 
   (* Boolean expressions *)
 
-  | Make_not :: instr, (Bool exp) :: stack, rest ->
-    loop (instr, Bool (Not exp) :: stack, rest)
+  | Make_not :: stack, (Bool exp) :: scratch, stream ->
+    loop (stack, Bool (Not exp) :: scratch, stream)
 
-  | Make_and :: instr, (Bool exp2) :: (Bool exp1) :: stack, rest ->
-    loop (instr, Bool (And (exp1, exp2)) :: stack, rest)
+  | Make_and :: stack, (Bool exp2) :: (Bool exp1) :: scratch, stream ->
+    loop (stack, Bool (And (exp1, exp2)) :: scratch, stream)
 
-  | Make_or :: instr, (Bool exp2) :: (Bool exp1) :: stack, rest ->
-    loop (instr, Bool (Or (exp1, exp2)) :: stack, rest)
+  | Make_or :: stack, (Bool exp2) :: (Bool exp1) :: scratch, stream ->
+    loop (stack, Bool (Or (exp1, exp2)) :: scratch, stream)
 
-  | Make_bool_op :: instr, (Arith exp2) :: (Bool_op op) :: (Arith exp1) :: stack, rest -> 
-    loop (instr, Bool (Bool_op (exp1, op, exp2)) :: stack, rest)
+  | Make_bool_op :: stack, (Arith exp2) :: (Bool_op op) :: (Arith exp1) :: scratch, stream -> 
+    loop (stack, Bool (Bool_op (exp1, op, exp2)) :: scratch, stream)
 
   (* Arithmetic *)
 
-  | Make_plus :: instr, (Arith exp2) :: (Arith exp1) :: stack, rest ->
-    loop (instr, Arith (Plus (exp1, exp2)) :: stack, rest)
+  | Make_plus :: stack, (Arith exp2) :: (Arith exp1) :: scratch, stream ->
+    loop (stack, Arith (Plus (exp1, exp2)) :: scratch, stream)
 
-  | Make_times :: instr, (Arith exp2) :: (Arith exp1) :: stack, rest ->
-    loop (instr, Arith (Times (exp1, exp2)) :: stack, rest)
+  | Make_times :: stack, (Arith exp2) :: (Arith exp1) :: scratch, stream ->
+    loop (stack, Arith (Times (exp1, exp2)) :: scratch, stream)
 
   (* Exception *)
-  | instr, stack, rest ->
-    raise @@ Parse_error (instr, stack, rest)
+  | stack, scratch, stream ->
+    raise @@ Parse_error (stack, scratch, stream)
 ;;
 
-let parse tokens =
+let parse token_stream =
   try
-    Result.return @@ loop ([Push_statement], [], tokens)
-  with Parse_error (instr, stack, rest) -> Result.fail (instr, stack, rest)
+    Result.return @@ loop ([Push_statement], [], token_stream)
+  with Parse_error (stack, scratch, stream) -> Result.fail (stack, scratch, stream)
 ;;
 
